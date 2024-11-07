45ef604013ab48e0ba426185e8f3627521d6abc6
Husna Ezzah <92300410+husnaezzah@users.noreply.github.com>
2024-11-08 00:15:39 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,30 +5,29 @@ import 'home_page.dart';
 import 'profile_page.dart';
 import 'capture_page.dart';
 import 'qa_page.dart';
-/*import 'read_page.dart';
-import 'qa_page.dart';
-import 'sos_page.dart';
-import 'assistance_page.dart'; */
 
 void main() {
-  runApp(MaterialApp(
-    title: 'BreakFree',
-    theme: ThemeData(
-      primarySwatch: Colors.purple,
-    ),
-    initialRoute: '/',
-    routes: {
-      '/': (context) => LoginPage(),
-      '/singup': (context) => SignUpPage(),
-      '/login': (context) => LoginPage(),
-      '/home': (context) => HomePage(),
-      '/profile': (context) => ProfilePage(),
-      '/capture': (context) => CapturePage(),
-      '/qa': (context) => QAPage(),
-      /*'/read': (context) => ReadPage(),
-      '/qa': (context) => QAPage(),
-      '/sos': (context) => SOSPage(),
-     '/assistance': (context) => AssistancePage(),*/
-    },
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BreakFree',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(),
+        '/signup': (context) => SignUpPage(),
+        '/login': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/profile': (context) => ProfilePage(),
+        '/capture': (context) => CapturePage(),
+        '/qa': (context) => QAPage(),
+      },
+    );
+  }
 }
